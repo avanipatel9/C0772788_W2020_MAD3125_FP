cd04136e03c426649a284832a21c2e2e66d9efaf
Avani Patel <anpatel36@yahoo.in>
2020-04-12 17:55:21 -0400

    [AP] Splash screen lasted for 5 seconds

--- app/src/main/java/com/avanipatel9/c0772788_w2020_mad3125_fp/SplashActivity.java
+++ app/src/main/java/com/avanipatel9/c0772788_w2020_mad3125_fp/SplashActivity.java
@@ -2,13 +2,44 @@ package com.avanipatel9.c0772788_w2020_mad3125_fp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private int SPLASH_TIME_OUT = 6000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Window window = getWindow();
+
+// clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+// finally change the color
+        window.setStatusBarColor(Color.WHITE);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent mIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mIntent);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
