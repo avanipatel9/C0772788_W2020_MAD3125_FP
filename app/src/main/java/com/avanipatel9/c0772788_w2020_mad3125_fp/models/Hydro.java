a647fdbf246d1ac6664ffbb9212ba3c84817f5a0
Avani Patel <anpatel36@yahoo.in>
2020-04-13 19:43:33 -0400

    [AP] Constructor added in Hydro class

--- app/src/main/java/com/avanipatel9/c0772788_w2020_mad3125_fp/models/Hydro.java
+++ app/src/main/java/com/avanipatel9/c0772788_w2020_mad3125_fp/models/Hydro.java
@@ -1,4 +1,20 @@
 package com.avanipatel9.c0772788_w2020_mad3125_fp.models;
 
+import java.util.Date;
+
 public class Hydro extends Bill {
+
+   private String agencyName;
+   private int unitsConsumed;
+
+    public Hydro(String billID, Date billDate, BillType billType, String agencyName, int unitsConsumed) {
+        super(billID, billDate, billType);
+        this.agencyName = agencyName;
+        this.unitsConsumed = unitsConsumed;
+    }
+
+    @Override
+    public Double calculateBill() {
+        return null;
+    }
 }
