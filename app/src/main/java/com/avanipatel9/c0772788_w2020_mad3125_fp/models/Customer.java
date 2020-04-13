e7f65d2c33e21ec54ba7fcadf5ef4c8040838dd2
Avani Patel <anpatel36@yahoo.in>
2020-04-13 17:52:39 -0400

    [AP] Customer class added

---
+++ app/src/main/java/com/avanipatel9/c0772788_w2020_mad3125_fp/models/Customer.java
@@ -0,0 +1,12 @@
+package com.avanipatel9.c0772788_w2020_mad3125_fp.models;
+
+import java.util.Dictionary;
+import java.util.HashMap;
+
+public class Customer {
+    private String customerID;
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private String emailID;
+}
