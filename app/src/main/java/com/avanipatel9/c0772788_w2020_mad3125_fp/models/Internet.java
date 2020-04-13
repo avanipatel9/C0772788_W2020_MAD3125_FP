de2ffcbf4b73886e702812061f395b053b9d23f5
Avani Patel <anpatel36@yahoo.in>
2020-04-13 19:31:43 -0400

    [AP] Constructor and getter setter added in internet class

--- app/src/main/java/com/avanipatel9/c0772788_w2020_mad3125_fp/models/Internet.java
+++ app/src/main/java/com/avanipatel9/c0772788_w2020_mad3125_fp/models/Internet.java
@@ -3,7 +3,28 @@ package com.avanipatel9.c0772788_w2020_mad3125_fp.models;
 import java.util.Date;
 
 public class Internet extends Bill {
+    private String internetProviderName;
+    private Double internetGBUsed;
 
+    public Internet(String billID, Date billDate, BillType billType, String internetProviderName, Double internetGBUsed) {
+        super(billID, billDate, billType);
+        this.internetProviderName = internetProviderName;
+        this.internetGBUsed = internetGBUsed;
+    }
 
-    
+    public String getInternetProviderName() {
+        return internetProviderName;
+    }
+
+    public void setInternetProviderName(String internetProviderName) {
+        this.internetProviderName = internetProviderName;
+    }
+
+    public Double getInternetGBUsed() {
+        return internetGBUsed;
+    }
+
+    public void setInternetGBUsed(Double internetGBUsed) {
+        this.internetGBUsed = internetGBUsed;
+    }
 }
