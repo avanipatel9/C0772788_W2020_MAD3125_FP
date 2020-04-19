11a0b9f663d0772df6423326f75d6cfd2d2796c5
Avani Patel <anpatel36@yahoo.in>
2020-04-19 04:07:51 -0400

    [AP] DatePicker added in AddNewInternetBill class

--- app/src/main/java/com/avanipatel9/c0772788_w2020_mad3125_fp/ui/AddNewInternetBillActivity.java
+++ app/src/main/java/com/avanipatel9/c0772788_w2020_mad3125_fp/ui/AddNewInternetBillActivity.java
@@ -1,7 +1,10 @@
 package com.avanipatel9.c0772788_w2020_mad3125_fp.ui;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.InputType;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +12,8 @@ import com.avanipatel9.c0772788_w2020_mad3125_fp.R;
 import com.avanipatel9.c0772788_w2020_mad3125_fp.models.Customer;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Calendar;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -28,11 +33,30 @@ public class AddNewInternetBillActivity extends AppCompatActivity {
 
     Customer customer;
 
+    private DatePickerDialog picker;
+    final Calendar calendar = Calendar.getInstance();
+    final int day = calendar.get(Calendar.DAY_OF_MONTH);
+    final int month = calendar.get(Calendar.MONTH);
+    final int year = calendar.get(Calendar.YEAR);
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_internet_bill);
         ButterKnife.inject(this);
         getSupportActionBar().setTitle(Html.fromHtml("<font color= '#FFFFFF'> Add Internet Bill </font>"));
+
+        edtBillDateInternet.setInputType(InputType.TYPE_NULL);
+        edtBillDateInternet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // date picker dialog
+                picker = new DatePickerDialog(AddNewInternetBillActivity.this,
+                        (view, year, monthOfYear, dayOfMonth) -> edtBillDateInternet.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year), year, month, day);
+                picker.show();
+            }
+        });
     }
 }
