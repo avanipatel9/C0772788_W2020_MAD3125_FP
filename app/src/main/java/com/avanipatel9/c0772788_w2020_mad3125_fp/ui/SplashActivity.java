e17ed2357664c5b11569df488e7a69aa2c3e9fbe
Avani Patel <anpatel36@yahoo.in>
2020-04-19 19:59:27 -0400

    [AP] status bar hidden and background changed on splash screen

--- app/src/main/java/com/avanipatel9/c0772788_w2020_mad3125_fp/ui/SplashActivity.java
+++ app/src/main/java/com/avanipatel9/c0772788_w2020_mad3125_fp/ui/SplashActivity.java
@@ -2,10 +2,12 @@ package com.avanipatel9.c0772788_w2020_mad3125_fp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -21,14 +23,22 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         Window window = getWindow();
 
-// clear FLAG_TRANSLUCENT_STATUS flag:
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//// clear FLAG_TRANSLUCENT_STATUS flag:
+//        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//
+//// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+//        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//
+//// finally change the color
+//        window.setStatusBarColor(Color.WHITE);
 
-// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        View decorView = getWindow().getDecorView();
+// Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+// Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
 
-// finally change the color
-        window.setStatusBarColor(Color.WHITE);
 
 
         new Handler().postDelayed(new Runnable() {
