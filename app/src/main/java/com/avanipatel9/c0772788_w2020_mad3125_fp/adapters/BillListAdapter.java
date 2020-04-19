d419116132e3b946c13085c0af7f57d1cbb74ee4
Avani Patel <anpatel36@yahoo.in>
2020-04-18 23:46:08 -0400

    [AP] Bill details layout modified

--- app/src/main/java/com/avanipatel9/c0772788_w2020_mad3125_fp/adapters/BillListAdapter.java
+++ app/src/main/java/com/avanipatel9/c0772788_w2020_mad3125_fp/adapters/BillListAdapter.java
@@ -9,6 +9,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.avanipatel9.c0772788_w2020_mad3125_fp.R;
@@ -17,11 +18,13 @@ import com.avanipatel9.c0772788_w2020_mad3125_fp.models.Hydro;
 import com.avanipatel9.c0772788_w2020_mad3125_fp.models.Internet;
 import com.avanipatel9.c0772788_w2020_mad3125_fp.models.Mobile;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
 
 public class BillListAdapter extends RecyclerView.Adapter<BillListAdapter.BillViewHolder> {
 
     ArrayList<Bill> billArrayList;
+    NumberFormat format = NumberFormat.getCurrencyInstance();
 
     public BillListAdapter(ArrayList<Bill> billArrayList) {
         this.billArrayList = billArrayList;
@@ -44,49 +47,54 @@ public class BillListAdapter extends RecyclerView.Adapter<BillListAdapter.BillVi
         {
             Hydro mHydro = (Hydro) mBill;
             holder.imgBill.setImageResource(R.drawable.hydro1);
-            holder.txtBillDate.setText(mBill.getBillDate().toString());
-            holder.txtBillType.setText(mBill.getBillType().toString());
-            holder.txtManufacturerName.setText("Agency Name");
-            holder.txtValManufacturerName.setText((mHydro.getAgencyName()));
-            holder.txtPlanName.setText("Units Consumed");
-            holder.txtValPlanName.setText(String.valueOf(mHydro.getUnitsConsumed()));
-            holder.txtBillAmount.setText(String.format("%2f", mBill.getBillAmount()));
+            holder.txtBillDate.setText(mHydro.getBillDate().toString());
+            holder.txtBillType.setText(mHydro.getBillType().toString());
+            holder.txtBillAmount.setText(format.format(mHydro.getBillAmount()));
+            holder.txtAgencyName.setText(mHydro.getAgencyName());
+            holder.txtUnitsConsumed.setText(String.format("%d units", mHydro.getUnitsConsumed()));
 
-            holder.ll5Bill.setVisibility(View.INVISIBLE);
-            holder.ll6Bill.setVisibility(View.INVISIBLE);
-            holder.ll7Bill.setVisibility(View.INVISIBLE);
+            holder.llMobile.setVisibility(View.INVISIBLE);
+            holder.llInternet.setVisibility(View.INVISIBLE);
+
         }
 
         else if(mBill.getBillID().contains("NET"))
         {
             Internet mInternet = (Internet) mBill;
             holder.imgBill.setImageResource(R.drawable.wifi1);
-            holder.ll5Bill.setVisibility(View.INVISIBLE);
-            holder.ll6Bill.setVisibility(View.INVISIBLE);
-            holder.ll7Bill.setVisibility(View.INVISIBLE);
+
             holder.txtBillDate.setText(mInternet.getBillDate().toString());
             holder.txtBillType.setText(mInternet.getBillType().toString());
-            holder.txtManufacturerName.setText("Internet Provider Name");
-            holder.txtValManufacturerName.setText(mInternet.getInternetProviderName());
-            holder.txtPlanName.setText("Internet GB Used");
-            holder.txtValPlanName.setText(String.valueOf(mInternet.getInternetGBUsed()));
-            holder.txtBillAmount.setText(String.format("%2f", mInternet.getBillAmount()));
+            holder.txtBillAmount.setText(format.format(mInternet.getBillAmount()));
+            holder.txtInternetProviderName.setText(mInternet.getInternetProviderName());
+            holder.txtInternetGBUsed.setText(mInternet.getInternetGBUsed() + "GB");
+
+            holder.llHydro.setVisibility(View.INVISIBLE);
+            holder.llMobile.setVisibility(View.INVISIBLE);
+
         }
 
-        else
+        else if(mBill.getBillID().contains("MOB"))
         {
             holder.imgBill.setImageResource(R.drawable.mobile1);
             Mobile mMobile = (Mobile) mBill;
             holder.txtBillDate.setText(mMobile.getBillDate().toString());
             holder.txtBillType.setText(mMobile.getBillType().toString());
-            holder.txtManufacturerName.setText("Manufacturer Name");
-            holder.txtValManufacturerName.setText(mMobile.getMobileManufacturerName());
-            holder.txtPlanName.setText("Plan Name");
-            holder.txtValPlanName.setText(mMobile.getPlanName());
+            holder.txtBillAmount.setText(format.format(mMobile.getBillAmount()));
+            holder.txtManufacturerName.setText(mMobile.getMobileManufacturerName());
+            holder.txtPlanName.setText(mMobile.getPlanName());
             holder.txtMobileNumber.setText(mMobile.getMobileNumber());
-            holder.txtInternetGBUsed.setText(String.valueOf(mMobile.getMobileGBUsed()));
-            holder.txtMinutesUsed.setText(String.valueOf(mMobile.getMinutesUsed()));
-            holder.txtBillAmount.setText(String.format("%2f", mMobile.getBillAmount()));
+            holder.txtMobileGBUsed.setText(mMobile.getMobileGBUsed() + "GB");
+            holder.txtMinutesUsed.setText(mMobile.getMinutesUsed() + "Minutes");
+
+            holder.llHydro.setVisibility(View.INVISIBLE);
+            holder.llInternet.setVisibility(View.INVISIBLE);
+
+        }
+        else
+        {
+            holder.txtTotalBillToPay.setText("This Customer has no bills");
+            holder.cardView.setVisibility(View.INVISIBLE);
         }
 
 
@@ -99,37 +107,48 @@ public class BillListAdapter extends RecyclerView.Adapter<BillListAdapter.BillVi
 
     public class BillViewHolder extends RecyclerView.ViewHolder {
 
-        LinearLayout ll5Bill;
-        LinearLayout ll6Bill;
-        LinearLayout ll7Bill;
+        CardView cardView;
+        LinearLayout llHydro;
+        LinearLayout llInternet;
+        LinearLayout llMobile;
+
+        TextView txtTotalBillToPay;
+        TextView txtBillAmount;
         TextView txtBillDate;
         TextView txtBillType;
-        TextView txtManufacturerName;
-        TextView txtValManufacturerName;
-        TextView txtPlanName;
-        TextView txtValPlanName;
-        TextView txtMobileNumber;
+        TextView txtAgencyName;
+        TextView txtUnitsConsumed;
         TextView txtInternetGBUsed;
+        TextView txtInternetProviderName;
+        TextView txtManufacturerName;
+        TextView txtPlanName;
+        TextView txtMobileNumber;
+        TextView txtMobileGBUsed;
         TextView txtMinutesUsed;
-        TextView txtBillAmount;
+
         ImageView imgBill;
 
         public BillViewHolder(@NonNull View itemView) {
             super(itemView);
-            this.ll5Bill = itemView.findViewById(R.id.ll_5_bill);
-            this.ll6Bill = itemView.findViewById(R.id.ll_6_bill);
-            this.ll7Bill = itemView.findViewById(R.id.ll_7_bill);
+
+            this.cardView = itemView.findViewById(R.id.card_view);
+            this.llHydro = itemView.findViewById(R.id.ll_bill_hydro);
+            this.llInternet = itemView.findViewById(R.id.ll_bill_internet);
+            this.llMobile = itemView.findViewById(R.id.ll_bill_Mobile);
+            this.imgBill = itemView.findViewById(R.id.image_bill);
+            this.txtBillAmount = itemView.findViewById(R.id.txt_val_bill_amount);
             this.txtBillDate = itemView.findViewById(R.id.txt_val_bill_date);
             this.txtBillType = itemView.findViewById(R.id.txt_val_bill_type);
-            this.txtManufacturerName = itemView.findViewById(R.id.txt_manufacturer_name);
-            this.txtValManufacturerName = itemView.findViewById(R.id.txt_val_manufacturer_name);
-            this.txtPlanName = itemView.findViewById(R.id.txt_plan_name);
-            this.txtValPlanName = itemView.findViewById(R.id.txt_val_plan_name);
+            this.txtAgencyName = itemView.findViewById(R.id.txt_val_agency_name);
+            this.txtUnitsConsumed = itemView.findViewById(R.id.txt_val_units_consumed);
+            this.txtInternetProviderName = itemView.findViewById(R.id.txt_val_internet_provider);
+            this.txtInternetGBUsed = itemView.findViewById(R.id.txt_val_internet_gb_used);
+            this.txtManufacturerName = itemView.findViewById(R.id.txt_val_manufacturer_name);
+            this.txtPlanName = itemView.findViewById(R.id.txt_val_plan_name);
             this.txtMobileNumber = itemView.findViewById(R.id.txt_val_mobile_number);
-            this.txtInternetGBUsed = itemView.findViewById(R.id.txt_val_mobile_gb_used);
+            this.txtMobileGBUsed = itemView.findViewById(R.id.txt_val_mobile_gb_used);
             this.txtMinutesUsed = itemView.findViewById(R.id.txt_val_minutes_used);
-            this.txtBillAmount = itemView.findViewById(R.id.txt_val_bill_amount);
-            this.imgBill = itemView.findViewById(R.id.image_bill);
+
         }
     }
 }
