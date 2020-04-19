841c961874c4b06dad5d8a2757bd40eea677f66d
Avani Patel <anpatel36@yahoo.in>
2020-04-19 04:34:06 -0400

    [AP] validations added for empty fields and bill id in AddNewMobileBillActivity

--- app/src/main/java/com/avanipatel9/c0772788_w2020_mad3125_fp/ui/AddNewMobileBillActivity.java
+++ app/src/main/java/com/avanipatel9/c0772788_w2020_mad3125_fp/ui/AddNewMobileBillActivity.java
@@ -69,5 +69,53 @@ public class AddNewMobileBillActivity extends AppCompatActivity {
                 picker.show();
             }
         });
+
+        btnSaveNewMobileBill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(edtBillIdMobile.getText().toString().isEmpty() && edtBillDateMobile.getText().toString().isEmpty() && edtMobileManufacturerName.getText().toString().isEmpty() && edtPlanName.getText().toString().isEmpty() && edtMobileNumber.getText().toString().isEmpty() && edtMobileGbUsed.getText().toString().isEmpty() && edtMinutesUsed.getText().toString().isEmpty())
+                {
+                    edtBillIdMobile.setError("Please Enter Bill ID");
+                    edtBillDateMobile.setError("Please Enter Bill Date");
+                    edtMobileManufacturerName.setError("Please Enter Mobile Manufacturer Name");
+                    edtPlanName.setError("Please Enter Internet GB Used");
+                    edtMobileNumber.setError("Please Enter Mobile Number");
+                    edtMinutesUsed.setError("Please Enter Minutes Used");
+                    edtMobileGbUsed.setError("Please Enter Internet GB Used");
+                }
+                else if(edtBillIdMobile.getText().toString().isEmpty())
+                {
+                    edtBillIdMobile.setError("Please Enter Bill ID");
+                }
+                else if(!edtBillIdMobile.getText().toString().contains("MOB"))
+                {
+                    edtBillIdMobile.setError("Bill ID should start with 'MOB'");
+                }
+                else if(edtBillDateMobile.getText().toString().isEmpty())
+                {
+                    edtBillDateMobile.setError("Please Enter Bill Date");
+                }
+                else if(edtMobileManufacturerName.getText().toString().isEmpty())
+                {
+                    edtMobileManufacturerName.setError("Please Enter Mobile Manufacturer Name");
+                }
+                else if(edtPlanName.getText().toString().isEmpty())
+                {
+                    edtPlanName.setError("Please Enter Plan Name");
+                }
+                else if(edtMobileNumber.getText().toString().isEmpty())
+                {
+                    edtMobileNumber.setError("Please Enter Mobile Number");
+                }
+                else if(edtMinutesUsed.getText().toString().isEmpty())
+                {
+                    edtMinutesUsed.setError("Please Enter Minutes Used");
+                }
+                else if(edtMobileGbUsed.getText().toString().isEmpty())
+                {
+                    edtMobileGbUsed.setError("Please Enter Internet GB Used");
+                }
+            }
+        });
     }
 }
