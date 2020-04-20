e1ec9dbcef2a15466ec693bcce9540a7a1d2f9f9
Avani Patel <anpatel36@yahoo.in>
2020-04-20 02:24:40 -0400

    [AP] splash screen created with kotlin

---
+++ app/src/main/java/com/avanipatel9/c0772788_w2020_mad3125_fp/ui/SplashScreenActivity.kt
@@ -0,0 +1,37 @@
+package com.avanipatel9.c0772788_w2020_mad3125_fp.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.Animation
+import android.widget.ImageView
+import com.avanipatel9.c0772788_w2020_mad3125_fp.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    
+    // This is the loading time of the splash screen
+    private val SPLASH_TIME_OUT:Long = 6000 // 2 sec
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        // Hide the status bar.
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+// Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this,LoginActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
